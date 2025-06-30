568405d2d83d44d3e9ef7f80b0fa3ff8b03d2bbe
Nazar Babii <babiyno@gmail.com>
2025-06-30 13:17:05 -0500

    JUnit results added

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['list']],
+  reporter: [
+    ['html'],
+    ['list'],
+    ['junit', { outputFile: 'test-results/junit-results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
